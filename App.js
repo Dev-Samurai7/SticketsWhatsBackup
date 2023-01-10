62949ee23248c1665fdd671b33ce14180898b77e
Jose Lourenco <josern1771@gmail.com>
2023-01-09 23:01:27 -0300

    Inicialização da aplicação

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Primeiro teste de criação do app SticketsWhatsapp Backup!</Text>
       <StatusBar style="auto" />
     </View>
   );
