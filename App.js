5ad1878e8fd54dade79325088e9c2c5a4d22ba03
José Lourenço <jose.lourenco@swa.com.br>
2023-01-10 15:44:27 -0300

    Acionar 'theme' aos botôes

--- App.js
+++ App.js
@@ -2,10 +2,26 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Image } from 'react-native';
 import ImageViewer from './components/ImageViewer';
 import Button from './components/Button';
+import * as ImagePicker from 'expo-image-picker';
 
 const iconWhats = require('./assets/img/iconWhats.jpg');
 
+
 export default function App() {
+  const pickImageAsync = async () => {
+
+    let result = await ImagePicker.launchImageLibraryAsync({
+      allowsEditing: true,
+      quality: 1
+    })
+
+    if (!result.canceled) {
+      console.log(result);
+    } else {
+      alert('Você não selecionou uma imagem!')
+    }
+  }
+
   return (
     <View style={styles.container}>
 
@@ -13,12 +29,12 @@ export default function App() {
         <ImageViewer iconWhats={iconWhats} />
       </View>
 
-      <Text> Export seu Stickets Whatsapp para fazer o Backup!</Text>
-      <Text> Impor seu Stickets Whatsapp para usar no seu dispositivo </Text>
+      <Text> Export seus Stickets Whatsapp para fazer o Backup!</Text>
+      <Text> Import seus Stickets Whatsapp para usar no seu dispositivo </Text>
 
       <View style={styles.footerContainer}>
-        <Button label="Import Stickets" />
-        <Button label="Export Stickets" />
+        <Button theme="primary" label="Import Stickets" />
+        <Button theme="primary" label="Export Stickets" />
       </View>
 
       <StatusBar style="auto" />
@@ -30,6 +46,7 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
+
     flex: 1,
     backgroundColor: 'green',
     alignItems: 'center',
@@ -38,6 +55,6 @@ const styles = StyleSheet.create({
   footerContainer: {
     flex: 1 / 3,
     alignItems: 'center',
-    textShadowColor : "black"
+    textShadowColor: "black"
   },
 });
