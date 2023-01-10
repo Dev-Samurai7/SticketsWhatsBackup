8c0dccdb88402548380e384bb4e741c7cc449b7d
José Lourenço <jose.lourenco@swa.com.br>
2023-01-10 15:46:06 -0300

    Adicionar theme aos botões

--- components/Button.js
+++ components/Button.js
@@ -1,11 +1,34 @@
 import { StyleSheet, View, Pressable, Text } from "react-native";
+import FontAwesome from "@expo/vector-icons/FontAwesome";
 
-export default function Button({ label }) {
+export default function Button({ label, theme, onPress }) {
+
+    if (theme === "primary") {
+        return (
+            <View
+                style={[styles.buttonContainer, { borderWidth: 6, borderColor: "#ffd33d", borderRadius: 20 }]}
+            >
+                <Pressable
+                    style={[styles.button, { backgroundColor: "#fff" }]}
+                    onPress={() => alert('botão acionado')}
+                >
+                    <FontAwesome
+                        name="picture-o"
+                        size={15}
+                        color="#25292e"
+                        style={styles.buttonIcon}
+                    />
+                    <Text style={[styles.buttonLabel, { color: "#25292e" }]}>{label}</Text>
+                </Pressable>
+            </View>
+        );
+    }
+
     return (
         <View style={styles.buttonContainer}>
-        <Pressable style={styles.button} onPress={() => alert('Você pressionou o botão')}>
-            <Text style={styles.buttonLabel}>{label}</Text>
-        </Pressable>
+            <Pressable style={styles.button} onPress={() => alert('Você pressionou o botão')}>
+                <Text style={styles.buttonLabel}>{label}</Text>
+            </Pressable>
         </View>
     );
 }
